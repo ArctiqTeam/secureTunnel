8dbfba63b3db5b68e503455d585026d854c672ae
root <root@mongodb-01.cdcug.local>
2017-11-11 12:58:54 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # secureTunnel
 secureTunnel - secure tunnel daemon
+
+## Requirements
+Below are the requirements needed to use the script:
+* [SSH keys](#SSH-Keys) are added to the target host
+* Script is run as root or user with sudo privileges
+
+## Usuage
+To use this application, run the script:
+```
+chmod u+x generate_service_file.py
+./generate_service_file.py
+```
+Once the script has been run, verify the service has successfully started:
+```
+systemctl status sercure-tunnel.service
+```
+The tunnel can also be verified by trying to pull a file that the target host is serving on the web (ex. the target host in the example has `bootstrap.py` located at `/var/www/html/pub/`:
+```
+curl -O localhost:8000/pub/bootstrap.py
+```
+
+## SSH Keys
+To add keys to the target host:
+1. Generate RSA key pair and follow steps (if does not exist):
+```
+ssh-keygen -t rsa
+```
+2. Copy the public key to the target host:
+```
+ssh-copy-id <user>@<target_host>
+```
---
+++ generate_service_file.py
@@ -0,0 +1,53 @@
+#!/bin/python
+from subprocess import call
+
+def file_create(server, user):
+    """ Create the service file
+    with user input.
+    """
+    file = open("secure-tunnel.service", "w")
+
+    print("Creating service file..."),
+
+    # Service file contents
+    file.write("""[Unit]
+    Description=secureTunnel - secure tunnel daemon
+    After=network.target
+    Documentation=https://github.com/ArctiqTeam/secureTunnel
+
+    [Service]
+    ExecStart=/usr/bin/ssh -NT -L 8001:{}:80 {}@{}
+    RestartSec=5
+    Restart=always
+
+    [Install]
+    WantedBy=multi-user.target""".format(server, user, server))
+
+    file.close()
+    print("[OK]")
+ 
+def main():
+    """ Start the program, call the
+    file_create function, move the service
+    file and then start it.
+    """
+    print ("Welcome to the secureTunnel generator.")
+
+    # Ask the user for some details
+    server = raw_input("Target host: ")
+    user = raw_input("User to login as: ")
+    
+    file_create(server, user)
+
+    # Move service file to proper location
+    print("Moving service file to '/etc/systemd/system/'..."),
+    call(["mv", "secure-tunnel.service", "/etc/systemd/system/"])
+    print("[OK]")
+
+    # Start the service
+    print("Starting the service..."),
+    call(["systemctl", "start", "secure-tunnel.service"])
+    print("[OK]")
+
+if __name__ == "__main__":
+   main()
